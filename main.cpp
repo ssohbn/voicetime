0e3e1495d542b4ec56ac6ddc08cf6f3858a9db72
m <92878876+ssohbn@users.noreply.github.com>
2025-09-21 19:05:52 -0700

    add recording lib and move stuff a little to prepare for maybe making a sound recorder program

--- main.cpp
+++ main.cpp
@@ -1,39 +1,38 @@
+#include "miniaudio.h"
 #include <FLAC++/metadata.h>
 #include <cstdint>
 #include <filesystem>
 #include <iostream>
 
-int main(int argc, char *argv[]) {
-  std::filesystem::path path(std::filesystem::current_path());
+double timeElapsed(std::filesystem::path directoryPath) {
 
   double duration_seconds = 0;
 
-  for (const auto &entry : std::filesystem::directory_iterator(path)) {
-
+  for (const auto &entry : std::filesystem::directory_iterator(directoryPath)) {
 
     // flac not found as extension
     if (entry.path().extension().string().find("flac") == -1) {
-	    std::cout << "file is not a flac: " << entry.path().string() << std::endl;
-	    continue;
+      continue;
     };
-
-    std::cout << entry.path().string() << std::endl;
 
     FLAC::Metadata::StreamInfo streaminfo;
     FLAC::Metadata::get_streaminfo(entry.path().string().c_str(), streaminfo);
-
-    std::cout << "looking at streaminfo" << std::endl;
 
     uint64_t samples = streaminfo.get_total_samples();
     uint32_t samplerate = streaminfo.get_sample_rate();
     duration_seconds +=
         static_cast<double>(samples) / static_cast<double>(samplerate);
-
-    std::cout << "samples: " << samples << std::endl;
-    std::cout << "samplerate: " << samplerate << std::endl;
   }
 
-  std::cout << "total duration voice trained: " << duration_seconds << std::endl;
+  return duration_seconds;
+};
+
+int main(int argc, char *argv[]) {
+  std::filesystem::path path(std::filesystem::current_path());
+
+  double elapsed = timeElapsed(path);
+
+  std::cout << "total duration voice trained: " << elapsed << std::endl;
 
   return 0;
 }
